c0c48d5fde40af3875639fd3db49c710c3a7db5d
Jay Thomas <jay@gfax.ch>
2021-09-11 22:39:51 -0400

    Get sample plugins working

--- guess.py
+++ guess.py
@@ -1,19 +1,45 @@
 import discord
 from discord.ext import commands
+import random
 
 class Guess(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.number = None
+        # The number the user is trying to guess
+        self.number = self.generate_number()
+        # Number of guesses made at getting the right number
+        self.attempts = 0
+
+    def generate_number(self):
+        return random.randint(0, 100)
 
     @commands.command(name='guess')
-    async def guess(self, ctx, *, member: discord.Member = None):
+    async def guess(self, ctx):
         """Guess the number"""
-        if not self.number:
-            self.number = random.randint(0, 100)
+        try:
+            guessed_number = int(ctx.message.clean_content.split(' ')[-1])
+        except Exception:
+            await ctx.send('Guess a number between 0 and 100')
+            return
 
-        ctx.send('Number is {0}'.format(self.number))
+        self.attempts += 1
 
-# We give bot.add_cog() the class name defined above
+        if guessed_number > self.number:
+            await ctx.send('Too high.')
+            return
+
+        if guessed_number < self.number:
+            await ctx.send('Too low.')
+            return
+
+        await ctx.send(
+            '{0}. You guessed right! Guesses made: {1}'.format(
+                self.number,
+                self.attempts
+            )
+        )
+        self.number = self.generate_number()
+        self.attempts = 0
+
 def setup(bot):
     bot.add_cog(Guess(bot))
--- rng.py
+++ rng.py
@@ -1,27 +1,57 @@
-import random
 from discord.ext import commands
+import random
 
 class RNG(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def roll(self, dice : str):
+    async def roll(self, ctx):
         """Rolls a dice in NdN format."""
+        #
+        # Sanity checks yay!
+        #
         try:
-            rolls, limit = map(int, dice.split('d'))
+            rolls, limit = map(int, ctx.message.content.split(' ')[-1].split('d'))
         except Exception:
-            await self.bot.say('Format has to be in NdN!')
+            await ctx.send('Format has to be in NdN!')
             return
 
-        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-        await self.bot.say(result)
+        if rolls < 1 or rolls > 100:
+            await ctx.send("That's an unreasonable number of dice")
+            return
+
+        if limit < 1:
+            await ctx.send("A die with that many sides breaks the fundamental laws of physics.")
+            return
+
+        if limit > 1000000:
+            await ctx.send('Ok a die with that many sides is clearly just a sphere.')
+            return
+
+        #
+        # Actual code
+        #
+        results = list(map(lambda x: random.randint(1, limit), range(rolls)))
+
+        if len(results) == 1:
+            await ctx.send(str(results[0]))
+            return
+
+        total = sum(results)
+        results = ' + '.join(map(str, results))
+        await ctx.send('{0} = {1}'.format(results, total))
 
     @commands.command(description='For when you wanna settle the score some other way')
-    async def choose(self, *choices : str):
+    async def choose(self, ctx):
         """Chooses between multiple choices."""
-        await self.bot.say(random.choice(choices))
+        [*choices] = ctx.message.clean_content.split(', ')
 
+        if len(choices) < 2:
+            await ctx.send("Tell me some choices you have, separated by commas. I'll choose one for you.")
+            return
+
+        await ctx.send(random.choice(choices))
 
 def setup(bot):
     bot.add_cog(RNG(bot))
