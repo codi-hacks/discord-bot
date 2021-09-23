7e41c1386f248507ff6ab69dddd7917427754b8c
Trevor Corcoran <42284150+SuperShadowPlay@users.noreply.github.com>
2021-09-23 13:03:18 -0400

    Improvments to guessing game and extensions (#6)
    * Added extensions folder and improved guessing game.
    
    * Fixed hard-coded references to command prefix in extensions/guess.py
    
    * Added Jay's patch and fixed auto-quit bug.
    
    * Removed useless import in guess.py

---
+++ extensions/guess.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import commands
+import random
+
+# Name of the command that activates the game.
+COMMAND = 'guess'
+
+class Guess(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        # The number the user is trying to guess
+        self.number = self.generate_number()
+        # Number of guesses made at getting the right number
+        self.attempts = 0
+        # Track who is actually playing the game.
+        self.guesser = ''
+        # Track if the game is active.
+        self.active = False
+
+    def generate_number(self):
+        return random.randint(0, 100)
+
+    @commands.command(name=COMMAND)
+    async def guess(self, ctx):
+        """Guess the number"""
+        self.guesser = ctx.author
+        self.active = True
+        await ctx.send('''Starting guessing game with <@!{}>
+Guess a number between 1 and 100. Stop guessing to quit.'''.format(ctx.author.id))
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.author == self.guesser and self.active == True and COMMAND not in ctx.content:
+            try:
+                guessed_number = int(ctx.content)
+            except ValueError:
+                await ctx.channel.send('Quitting guessing game...')
+                self.number = self.generate_number()
+                self.attempts = 0
+                self.active = False
+                return
+
+            self.attempts += 1
+
+            if guessed_number > self.number:
+                await ctx.channel.send('Too high.')
+                return
+
+            if guessed_number < self.number:
+                await ctx.channel.send('Too low.')
+                return
+
+            await ctx.channel.send(
+                '{0}. You guessed right! Guesses made: {1}'.format(
+                    self.number,
+                    self.attempts
+                )
+            )
+            self.number = self.generate_number()
+            self.attempts = 0
+            self.active = False
+        return
+
+def setup(bot):
+    bot.add_cog(Guess(bot))
--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import discord
 from discord.ext import commands
 import yaml
+import os
 
 with open('config.yml', 'r') as file:
   config = yaml.safe_load(file)
 
-def get_prefix(bot, message):
+def get_prefix():
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
     # Prefixes may also contain spaces
     prefixes = ['.']
 
-    return commands.when_mentioned_or(*prefixes)(bot, message)
+    return commands.when_mentioned_or(*prefixes)
 
 #bot = discord.Client()
-bot = commands.Bot(command_prefix='.', description='CodiHacks bot')
+bot = commands.Bot(command_prefix=get_prefix(), description='CodiHacks bot')
 
 # Load cogs by the filenames
-bot.load_extension('guess')
-bot.load_extension('rng')
+for i in os.listdir('extensions'):
+	if i.endswith('.py') and os.path.isfile('extensions/' + i):
+		bot.load_extension('extensions.' + i.replace('.py', ''))
 
 # Event listeners
 @bot.event
