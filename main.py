2efa6f842cebf5db9c501e3421eded68d4eca912
Trevor Corcoran <42284150+SuperShadowPlay@users.noreply.github.com>
2021-10-07 11:52:40 -0400

    Example extension file (#8)
    * Added extensions folder and improved guessing game.
    
    * Created example extension file.
    
    * Watermelon emojis :watermelon:
    
    * Added extension counter as an extra
    
    * Changed on_message context variable name.

--- main.py
+++ main.py
@@ -18,14 +18,18 @@ def get_prefix():
 bot = commands.Bot(command_prefix=get_prefix(), description='CodiHacks bot')
 
 # Load cogs by the filenames
+extensionCounter = 0
 for i in os.listdir('extensions'):
-	if i.endswith('.py') and os.path.isfile('extensions/' + i):
-		bot.load_extension('extensions.' + i.replace('.py', ''))
+    if i.endswith('.py') and os.path.isfile('extensions/' + i):
+        bot.load_extension('extensions.' + i.replace('.py', ''))
+        extensionCounter += 1
 
 # Event listeners
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print('Logged in as {0.user}'.format(bot))
+    print('Loaded {} extensions'.format(extensionCounter))
+    print('==========')
 
 # Initialize bot
 bot.run(config['discord_token'], bot=True, reconnect=True)
